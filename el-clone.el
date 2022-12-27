8e50a82d55c4e22968f33f07a4883715ddaa18e2
takeokunn <bararararatty@gmail.com>
2022-12-27 12:32:46 +0900

    Add el-clone.el

---
+++ el-clone.el
@@ -0,0 +1,34 @@
+;;; el-clone.el --- package-vc wrapper -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022  Takeo Obara
+
+;; Author: Takeo Obara <bararararatty@gmail.com>
+;; Version: 1.0.0
+;; Keywords: package-manager
+;; Package-Requires: ((emacs "29.0"))
+;; URL: https://github.com/takeokunn/el-clone
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Generate yasnippet templates from org document.
+
+;;; Code:
+
+(message "Hello World!")
+
+(provide 'el-clone)
+
+;;; el-clone.el ends here
